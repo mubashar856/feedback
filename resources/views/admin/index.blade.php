923165ac432c9e2ae9a9b83ceedea21c8391951e
mubashar856 <mubasharahassan@gmail.com>
2016-10-21 10:06:02 +0500

    animate adminpanel

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -9,7 +9,7 @@
         <h2 class="admin-h2">
             WELCOME TO
         </h2>
-        <h1 class="admin-h1 animated rubberBand">
+        <h1 class="admin-h1 animated flipInX">
             ADMIN PANEL
         </h1>
     </div>
