e2010df5d621a15e22e8d5811578a3001687e928
mubashar856 <mubasharahassan@gmail.com>
2016-10-22 12:53:59 +0500

    added some style in index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,70 +1,90 @@
 @extends('layout')
 
 @section('title')
-  Feedback System
+    Feedback System
 @endsection
 
 
 
 @section('content')
 
-<div class="main-header">
-  <div class="main-header-inner">
-    <div class="search-box">
-      <form method="post" action="/">
-        <input type="text" name="teacher_name" class="search-field" placeholder="Teacher name">
-        <input type="hidden" name="_token" value="{{ csrf_token() }}">
-        <button type="submit" class="search-btn">Search</button>
-      </form>
+    <div class="main-header">
+        <div class="main-header-inner">
+            <div class="search-header">
+                <h1 class="main-search-heading animated">Search Teacher</h1>
+                <div class="search-box animated">
+                    <form method="post" action="/">
+                        <input type="text" name="teacher_name" class="search-field" placeholder="Teacher name">
+                        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                        <button type="submit" class="search-btn">Search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
 
-<br />
-<div class="container">
-  <ol class="breadcrumb">
-    <li class="active">Home</li>
-  </ol>
+    <br />
+    <div class="container">
+        <ol class="breadcrumb">
+            <li class="active">Home</li>
+        </ol>
 
 
-  <div class="panel panel-primary">
-    <div class="panel-heading">
-      <h3 class="panel-title">Search Results</h3>
+        <div class="panel panel-primary">
+            <div class="panel-heading">
+                <h3 class="panel-title">Search Results</h3>
+            </div>
+            <div class="panel-body">
+                <div class="row">
+                    @if(isset($teachers))
+                        @if(count($teachers))
+                            @foreach($teachers as $teacher)
+                                <div class="col-lg-3 col-md-4 col-sm-6">
+                                    <div class="teacher-result">
+                                        <div class="teacher-result-outer">
+                                            <div class="teacher-result-inner">
+                                                <img src="{{ asset('uploads/teacher/'. $teacher->teacher_picture) }}" class="teacher-search-img"><br /><br />
+                                                <span class="teacher-search-name">
+                                                    {{ $teacher->teacher_name }}
+                                                </span><br />
+                                                <span class="department-search-name">
+                                                    {{ $teacher->department->department_name }}
+                                                </span><br />
+                                                <a href="/teacher/{{ $teacher->slug }}" class="teacher-search-visit">profile</a>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @else
+                            <div class="alert alert-warning"><b>Warning! </b>No Result found.</div>
+                        @endif
+
+                    @else
+                        <div class="alert alert-info"><b>Hey Listen: </b>Enter the name of the teacher in above field to search.</div>
+                    @endif
+                </div><!-- row class end -->
+
+            </div>
+        </div><!-- panel end -->
+
     </div>
-    <div class="panel-body">
-      <div class="row">
-        @if(isset($teachers))
-          @if(count($teachers))
-	          @foreach($teachers as $teacher)
-	          <div class="col-lg-3 col-md-4 col-sm-6">
-	            <div class="teacher-result">
-	              <div class="teacher-result-outer">
-	                <div class="teacher-result-inner">
-	                  <img src="{{ asset('uploads/teacher/'. $teacher->teacher_picture) }}" class="teacher-search-img"><br /><br />
-	                  <span class="teacher-search-name">
-	                    {{ $teacher->teacher_name }}
-	                  </span><br />
-	                  <span class="department-search-name">
-	                    {{ $teacher->department->department_name }}
-	                  </span><br />
-	                  <a href="/teacher/{{ $teacher->slug }}" class="teacher-search-visit">profile</a>
-	                </div>
-	              </div>
-	            </div>
-	          </div>
-	          @endforeach
-          @else
-          	<div class="alert alert-warning"><b>Warning! </b>No Result found.</div>
-          @endif
 
-        @else
-        	<div class="alert alert-info"><b>Hey Listen: </b>Enter the name of the teacher in above field to search.</div>
-        @endif
-      </div><!-- row class end -->
+@endsection
 
-    </div>
-  </div><!-- panel end -->
 
-</div>
+@section('customJs')
+    <script>
+        $(document).ready(function(){
+            $('.search-box').mouseenter(function(){
+                $('.main-search-heading').addClass('fadeOutUp');
+                $('.main-search-heading').removeClass('fadeInDown');
+            });
+            $('.search-box').mouseleave(function(){
+                $('.main-search-heading').removeClass('fadeOutUp');
+                $('.main-search-heading').addClass('fadeInDown');
+            });
+        });
 
+    </script>
 @endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -10,24 +10,30 @@
     <!-- Bootstrap -->
     <link href="{{ asset('assets/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
 
+
     <!-- custom -->
     <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
+
+    {{--animate--}}
+    <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/animate.css') }}">
+
 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
-  <body>
-    
-    @yield('content')
+</head>
+<body>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="{{ asset('assets/js/jquery.js') }}"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
-  </body>
+@yield('content')
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="{{ asset('assets/js/jquery.js') }}"></script>
+n<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="{{ asset('assets/bootstrap/js/bootstrap.min.js') }}"></script>
+@yield('customJs')
+</body>
 </html>
